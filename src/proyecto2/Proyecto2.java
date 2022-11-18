172f8e0981ed5f4b3cbf3ec50f3dd80b4a93e7ac
JorshuaSolorzano22 <111464031+JorshuaSolorzano22@users.noreply.github.com>
2022-11-18 13:50:33 -0600

    Se agrega el metodo para leer el .txt

--- src/proyecto2/Proyecto2.java
+++ src/proyecto2/Proyecto2.java
@@ -1,12 +1,27 @@
 
 package proyecto2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class Proyecto2 {
-
-
     public static void main(String[] args) {
-        System.out.println(" Hola mundo");
+         try {
+      File myObj = new File("Lista.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("A ocurrido un error");
+      e.printStackTrace();
+    }
     }
     
+        
+         
 }
