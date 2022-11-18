671d6d3d963218bb1921a3d8c582fef99dfff64f
JorshuaSolorzano22 <111464031+JorshuaSolorzano22@users.noreply.github.com>
2022-11-18 15:56:38 -0600

    Se agrega la estructura del menú

--- src/proyecto2/LeerArchivos.java
+++ src/proyecto2/LeerArchivos.java
@@ -1,10 +1,12 @@
 package proyecto2;
+
 import java.io.File;  // Importa la clase File
 import java.io.FileNotFoundException;  // Importar esta clase para manejar errores
 import java.util.Scanner; // Importar la clase Scanner para leer archivos de texto
 
 public class LeerArchivos {
-  public static void leer() {
+    
+  public void leer() {
     try {
       File myObj = new File("Lista.txt");
       Scanner myReader = new Scanner(myObj);
@@ -18,4 +20,6 @@ public class LeerArchivos {
       e.printStackTrace();
     }
   }
+  
+  
 }
--- src/proyecto2/Proyecto2.java
+++ src/proyecto2/Proyecto2.java
@@ -4,14 +4,32 @@ package proyecto2;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 
 public class Proyecto2 {
-    public static void main(String[] args) {
-     LeerArchivos read = new LeerArchivos();
-        System.out.println(read);
-    }
     
+    public static void main(String[] args) {
+        LeerArchivos read = new LeerArchivos();
         
-         
+        int option;
+        option = Integer.parseInt(JOptionPane.showInputDialog(null, "Bienvenido al sistema\n" + "1. Ver la lista de estudiantes\n 2. Editar el archivo\n 3. Enviar por correo\n 4.Manual de usuario\n 4.Salir"));
+        do{
+            switch(option){
+                case 1:
+                  read.leer();
+                break;
+                case 2:
+                 
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+                case 5:
+                break;
+            }
+        }while(option != 4);
+        
+    }       
 }
