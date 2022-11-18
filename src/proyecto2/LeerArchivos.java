95f10b0b933ea7a29c0d13443aefdc101a552f4b
JorshuaSolorzano22 <111464031+JorshuaSolorzano22@users.noreply.github.com>
2022-11-18 16:25:19 -0600

    Se agrega el manual de usuario

--- src/proyecto2/LeerArchivos.java
+++ src/proyecto2/LeerArchivos.java
@@ -2,23 +2,41 @@ package proyecto2;
 
 import java.io.File;  // Importa la clase File
 import java.io.FileNotFoundException;  // Importar esta clase para manejar errores
-import java.util.Scanner; // Importar la clase Scanner para leer archivos de texto
+import java.io.BufferedReader; // Importar la clase Scanner para leer archivos de texto
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LeerArchivos {
     
-  public void leer() {
-    try {
+  public void leer() throws FileNotFoundException, IOException {
+      String data;
+      BufferedReader myReader = new BufferedReader (new FileReader("Lista.txt"));
       File myObj = new File("Lista.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
+      
+    try {
+      while ((data = myReader.readLine()) != null) {
         System.out.println(data);
       }
-      myReader.close();
     } catch (FileNotFoundException e) {
       System.out.println("A ocurrido un error");
       e.printStackTrace();
     }
+   
+  }
+    public void manual() throws FileNotFoundException, IOException {
+      String data;
+      BufferedReader myReader = new BufferedReader (new FileReader("Manual.txt"));
+      File myObj = new File("Manual.txt");
+      
+    try {
+      while ((data = myReader.readLine()) != null) {
+        System.out.println(data);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("A ocurrido un error");
+      e.printStackTrace();
+    }
+   
   }
   
   
--- src/proyecto2/Proyecto2.java
+++ src/proyecto2/Proyecto2.java
@@ -3,15 +3,16 @@ package proyecto2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 
 public class Proyecto2 {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         LeerArchivos read = new LeerArchivos();
-        
+        read.leer();
         int option;
         option = Integer.parseInt(JOptionPane.showInputDialog(null, "Bienvenido al sistema\n" + "1. Ver la lista de estudiantes\n 2. Editar el archivo\n 3. Enviar por correo\n 4.Manual de usuario\n 4.Salir"));
         do{
@@ -19,12 +20,14 @@ public class Proyecto2 {
                 case 1:
                   read.leer();
                 break;
+                
                 case 2:
                  
                 break;
                 case 3:
                 break;
                 case 4:
+                    read.manual();
                 break;
                 case 5:
                 break;
