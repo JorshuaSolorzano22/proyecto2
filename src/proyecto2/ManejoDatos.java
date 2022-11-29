bfd8752530feee615adacf62dd6aa00e1933e394
JorshuaSolorzano22 <111464031+JorshuaSolorzano22@users.noreply.github.com>
2022-11-29 15:11:46 -0600

    Se agrega la opcion agregar estudiante(FINAL)

--- src/proyecto2/ManejoDatos.java
+++ src/proyecto2/ManejoDatos.java
@@ -1,4 +1,3 @@
-
 package proyecto2;
 
 import java.io.BufferedReader;
@@ -17,35 +16,34 @@ import javax.swing.JOptionPane;
  * @author Jorshua Solorzano
  */
 public class ManejoDatos {
+
     File archivo = new File("Lista.txt");
     String carnet = " ";
     String name = " ";
     String gender = " ";
     String location = " ";
     PrintWriter linea;
-    
-    
-    
-//        public void leer() throws FileNotFoundException, IOException {
-//        String data;
-//        BufferedReader myReader = new BufferedReader(new FileReader("Lista.txt"));
-//        File myObj = new File("Lista.txt");
-//
-//        try {
-//            while ((data = myReader.readLine()) != null) {
-//                System.out.println(data);
-//            }
-//            myReader.close();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("A ocurrido un error");
-//            e.printStackTrace();
-//        }
-//
-//    }
+
+    public void leer() throws FileNotFoundException, IOException {
+        String data;
+        BufferedReader myReader = new BufferedReader(new FileReader("Lista.txt"));
+        File myObj = new File("Lista.txt");
+
+        try {
+            while ((data = myReader.readLine()) != null) {
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("A ocurrido un error");
+            e.printStackTrace();
+        }
+
+    }
 
     public void EditFiles() throws IOException {
         int menu;
-        
+
         do {
             menu = Integer.parseInt(JOptionPane.showInputDialog(null, "¿Que desea realizar con los grupos?\n"
                     + "1. Agregar estudiante\n 2. Ordenar lista de manera ascendente\n"
@@ -53,31 +51,31 @@ public class ManejoDatos {
                     + "5. Dividir el grupo\n 6. Eliminar estudiante \n 7. Salir"));
             switch (menu) {
                 case 1:
-                {
-                    try{
-               if(archivo.exists()){
-                   carnet = JOptionPane.showInputDialog(null, "Ingrese el carnet" 
-                   , "Solicitud de datos" , 3);
-                    name = JOptionPane.showInputDialog(null, "Ingrese el nombre" 
-                   , "Solicitud de datos" , 3);
-                     gender = JOptionPane.showInputDialog(null, "Ingrese el genero" 
-                   , "Solicitud de datos" , 3);
-                     location = JOptionPane.showInputDialog(null, "Ingrese la ubicacion" 
-                   , "Solicitud de datos" , 3);
-                   try (FileWriter escribir = new FileWriter(archivo, true)) {
-                       linea.print(carnet);
-                       linea.print(name);
-                       linea.print(gender);
-                       linea.print(location);
-                       linea.close();
-                   }
-               
-               }
-                  }catch (IOException ex) {
-                        Logger.getLogger(ManejoDatos.class.getName()).log(Level.SEVERE, null, ex);
-                       }  
-                }
-                break;
+
+                    if (archivo.exists()) {
+                        try {
+                            carnet = JOptionPane.showInputDialog(null, "Ingrese el carnet",
+                                    "C28464,");
+                            name = JOptionPane.showInputDialog(null, "Ingrese el nombre completo",
+                                    "Apellido 1_apellido2_nombre,");
+                            gender = JOptionPane.showInputDialog(null, "Ingrese el genero",
+                                    "Femenino o masculino,");
+                            location = JOptionPane.showInputDialog(null, "Ingrese la ubicacion",
+                                    "Neilly");
+                            FileWriter escribir = new FileWriter(archivo, true);
+                            PrintWriter linea = new PrintWriter(escribir);
+                            linea.print(carnet + "       ");
+                            linea.print(name + "       ");
+                            linea.print(gender + "       ");
+                            linea.print(location + "       " + "\n");
+                            linea.close();
+                            escribir.close();
+                        } catch (IOException ex) {
+                            Logger.getLogger(Proyecto2.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+
+                    break;
                 case 2:
                     break;
                 case 3:
@@ -87,23 +85,23 @@ public class ManejoDatos {
                 case 5:
                     break;
                 case 6:
-                    break;   
+                    break;
             }
         } while (menu != 7);
     }
 
-//    public void manual() throws FileNotFoundException, IOException {
-//        String data;
-//        BufferedReader myReader = new BufferedReader(new FileReader("Manual.txt"));
-//        File myObj = new File("Manual.txt");
-//        try {
-//            while ((data = myReader.readLine()) != null) {
-//                System.out.println(data);
-//            }
-//        } catch (FileNotFoundException e) {
-//            System.out.println("A ocurrido un error");
-//            e.printStackTrace();
-//        }
-//
-//    }
+    public void manual() throws FileNotFoundException, IOException {
+        String data;
+        BufferedReader myReader = new BufferedReader(new FileReader("Manual.txt"));
+        File myObj = new File("Manual.txt");
+        try {
+            while ((data = myReader.readLine()) != null) {
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("A ocurrido un error");
+            e.printStackTrace();
+        }
+
+    }
 }
